4ba443ed504664aed7099995da51137bc2d4897b
Chester-Francisco <113727433+Chester-Francisco@users.noreply.github.com>
2024-02-04 16:28:06 -0800

    Lab 4 complete

--- Assets/Scripts/FPSInput.cs
+++ Assets/Scripts/FPSInput.cs
@@ -5,18 +5,48 @@ using UnityEngine;
 public class FPSInput : MonoBehaviour {
 
     private float speed = 9.0f;
+    private float gravity = -9.8f;
+
+    private CharacterController charController;
 
     // Start is called before the first frame update
     void Start() {
-        
+        charController = GetComponent<CharacterController> ();
     }
 
     // Update is called once per frame
     void Update() {
+
+        /**     Using transform.Translate(movement) - without Rigidbody or Character controller class
+            float horizInput = Input.GetAxis("Horizontal");
+            float vertInput = Input.GetAxis("Vertical");
+            Vector3 movement = new Vector3(horizInput, 0, vertInput) * Time.deltaTime * speed;
+
+            transform.Translate(movement);
+        */
+
+        //Using Unity's built in CHaracterController with Collision Detection
         float horizInput = Input.GetAxis("Horizontal");
         float vertInput = Input.GetAxis("Vertical");
-        Vector3 movement = new Vector3(horizInput, 0, vertInput) * Time.deltaTime * speed;
+        Vector3 movement = new Vector3 (horizInput, 0, vertInput);
 
-        transform.Translate(movement);
+        // Clamp magnitude to limit diagonal movement
+        movement = Vector3.ClampMagnitude (movement, 1.0f);
+
+        // take speed into account
+        movement *= speed;
+
+        //adding gravity to 'push' player to the ground 
+        movement.y = gravity;
+        
+        // make movement processor independent
+        movement *= Time.deltaTime;
+
+        // Convert local to global coordinates
+        movement = transform.TransformDirection (movement);
+
+        //moving player using built in Character Controller
+        //notice the difference compared to rigidbody movement and transform.Translate(movement)
+        charController.Move (movement);
     }
 }
