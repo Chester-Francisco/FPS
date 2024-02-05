e07b8fde7a24234b6602750853a7f8e8418cee01
Chester-Francisco <113727433+Chester-Francisco@users.noreply.github.com>
2024-02-05 15:14:23 -0800

    Step 15 Completed
    End of Page 12

--- Assets/Scripts/ReactiveTarget.cs
+++ Assets/Scripts/ReactiveTarget.cs
@@ -2,11 +2,17 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class ReactiveTarget : MonoBehaviour
-{
+public class ReactiveTarget : MonoBehaviour { 
+ 
     public void ReactToHit() {
+        WanderingAI enemyAi = GetComponent<WanderingAI>();
+        if(enemyAi != null ) {
+            enemyAi.ChangeState(EnemyStates.dead);
+        }
+
         StartCoroutine (Die());
     }
+
     private IEnumerator Die() {
         // Enemy falls over and disappears after two seconds
         iTween.RotateAdd (this.gameObject, new Vector3 (-75, -0, 0), 1);
--- Assets/Scripts/WanderingAI.cs
+++ Assets/Scripts/WanderingAI.cs
@@ -2,38 +2,66 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+
+public enum EnemyStates { alive, dead};
+
+
 public class WanderingAI : MonoBehaviour {
 
 
     private float enemySpeed = 3.0f;
     private float obstacleRange = 5.0f;
     private float sphereRadius = 0.75f;
+    private EnemyStates state;
+
+    [SerializeField] private GameObject laserbeamPrefab;
+    private GameObject laserbeam;
+
+    public float fireRate = 2.0f;
+    private float nextFire = 0.0f;
 
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        state = EnemyStates.alive;
     }
 
     // Update is called once per frame
     void Update() {
 
-        //Move Enenmy
-        Vector3 movement = Vector3.forward * enemySpeed * Time.deltaTime;
-        transform.Translate(movement);
 
-        //generate ray
-        Ray ray = new Ray (transform.position, transform.forward);
+        if (state == EnemyStates.alive) { 
+            //Move Enenmy
+            Vector3 movement = Vector3.forward * enemySpeed * Time.deltaTime;
+            transform.Translate(movement);
 
-        // Spherecast and determine if the enemy needs to turn
-        RaycastHit hit;
-        if (Physics.SphereCast(ray, sphereRadius, out hit)) {
-            if (hit.distance < obstacleRange) {
-                float turnAngle = Random.Range(-110, 110);
-                transform.Rotate(Vector3.up * turnAngle);
+            //generate ray
+            Ray ray = new Ray(transform.position, transform.forward);
+
+            // Spherecast and determine if the enemy needs to turn
+            RaycastHit hit;
+
+            if (Physics.SphereCast(ray, sphereRadius, out hit)) {
+
+                GameObject hitObject = hit.transform.gameObject;
+
+                if(hitObject.GetComponent<PlayerCharacter> ()) {
+                    nextFire = Time.time + fireRate;
+                    laserbeam = Instantiate (laserbeamPrefab) as GameObject;
+                    laserbeam.transform.position = transform.TransformPoint(0, 1.5f, 1.5f);
+                    laserbeam.transform.rotation = transform.rotation;
+                }
+                else if  (hit.distance < obstacleRange) {
+                    float turnAngle = Random.Range(-110, 110);
+                    transform.Rotate(0, turnAngle, 0);
+                }
             }
         }
 
+    }
+
+    public void ChangeState(EnemyStates state ) { 
+        this.state = state;
     }
 }
